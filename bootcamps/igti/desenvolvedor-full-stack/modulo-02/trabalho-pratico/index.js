26a000686a955ca4c989e9e896b7ab3a0e97bdb3
Cristian J. Ambrosi <cris_ambrosi@hotmail.com>
2020-08-03 20:26:11 -0300

    Atualizado funcao para questao 3 e 4 do trabalho pratico

--- bootcamps/igti/desenvolvedor-full-stack/modulo-02/trabalho-pratico/index.js
+++ bootcamps/igti/desenvolvedor-full-stack/modulo-02/trabalho-pratico/index.js
@@ -2,71 +2,86 @@ import { promises as fs } from 'fs';
 
 // Question 1
 const citiesByStateGenerate = async () => {
-    try {
-        let citiesPerState = [];
-        const states = JSON.parse(await fs.readFile('./src/Estados.json'));
-        const cities = JSON.parse(await fs.readFile('./src/Cidades.json'));
+  try {
+    let citiesPerState = [];
+    const states = JSON.parse(await fs.readFile('./src/Estados.json'));
+    const cities = JSON.parse(await fs.readFile('./src/Cidades.json'));
 
-        states.forEach(state => {
-            citiesPerState = cities.filter(city => city.Estado === state.ID);
-            fs.writeFile(`./temp/${state.Sigla}.json`, JSON.stringify(citiesPerState, null, 2));
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
+    states.forEach((state) => {
+      citiesPerState = cities.filter((city) => city.Estado === state.ID);
+      fs.writeFile(
+        `./temp/${state.Sigla}.json`,
+        JSON.stringify(citiesPerState, null, 2)
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Question 2
-const readState = async stateUf => {
+const readState = async (stateUf) => {
+  try {
     let file = JSON.parse(await fs.readFile(`./temp/${stateUf}.json`));
     console.log(`Quantidade de Cidades no Estado de ${stateUf}: `, file.length);
-}
+  } catch (error) {
+    console.log(`Arquivo ${stateUf}.json não encontrado.`);
+  }
+};
 
 // Questions 3 and 4
 const filterCities = async (less, text) => {
-    const states = JSON.parse(await fs.readFile('./src/Estados.json'));
-    const cities = JSON.parse(await fs.readFile('./src/Cidades.json'));
-    const moreCitiesDec = [];
+  const states = JSON.parse(await fs.readFile('./src/Estados.json'));
+  const cities = JSON.parse(await fs.readFile('./src/Cidades.json'));
+  const moreCitiesDec = [];
 
-    const arrCities = states.map(state => {
-        const citiesPerState = cities.filter(city => city.Estado === state.ID);
-        
-        return {
-            uf: state.Sigla,
-            cities: citiesPerState
-        }
+  const arrCities = states
+    .map((state) => {
+      const citiesPerState = cities.filter((city) => city.Estado === state.ID);
+
+      return {
+        uf: state.Sigla,
+        cities: citiesPerState,
+      };
     })
     .sort((a, b) => {
-        if (less) [a, b] = [b, a];
+      if (less) [a, b] = [b, a];
 
-        return b.cities.length - a.cities.length;
+      return b.cities.length - a.cities.length;
     })
     .slice(0, 5);
 
-    //fs.writeFile(`./temp/moreCities.json`, JSON.stringify(arrCities, null, 2));
+  arrCities.sort((a, b) => {
+    return b.cities.length - a.cities.length;
+  });
 
-    arrCities.forEach(item => {
-        moreCitiesDec.push(`${item.uf} - ${item.cities.length}`);
-    });
+  arrCities.forEach((item) => {
+    moreCitiesDec.push(`${item.uf} - ${item.cities.length}`);
+  });
 
-    console.log(`Cinco estados com ${text} número de cidades são: `, moreCitiesDec);
-}
+  console.log(
+    `Cinco estados com ${text} número de cidades são: `,
+    moreCitiesDec
+  );
+
+  //fs.writeFile(`./temp/moreCities.json`, JSON.stringify(arrCities, null, 2));
+};
 
 // Questions 5 and 6
 const cityWidthName = async () => {
-    const states = JSON.parse(await fs.readFile('./src/Estados.json'));
-    let data = [];
+  const states = JSON.parse(await fs.readFile('./src/Estados.json'));
+  let data = [];
 
-    for (let state of states) {
-        data.push();
-    }
-    console.log(data);
-}
+  for (let state of states) {
+    data.push();
+  }
+  console.log(data);
+};
 
 // Questions 7 and 8
 
 // citiesByStateGenerate();
 readState('RS');
-filterCities(false,'maior');
+filterCities(false, 'maior');
 filterCities(true, 'menor');
 // cityWidthName();
