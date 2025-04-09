87fe4c93e311807441b14ad0c9b818a29b932774
Cristian J. Ambrosi <cjambrosi@outlook.com>
2025-04-08 22:33:33 -0300

    feat(full cycle): payment gateway event - day 1 start project

---
+++ events/full-cycle/payment-gateway/api/internal/domain/errors.go
@@ -0,0 +1,14 @@
+package domain
+
+import "errors"
+
+var (
+	// ErrAccountNotFound is returned when an account is not found
+	ErrAccountNotFound = errors.New("account not found")
+	// ErrDuplicateAPIKey is returned when an API key already exists
+	ErrDuplicateAPIKey = errors.New("api key already exists")
+	// ErrInvoiceNotFound is returned when an invoice is not found
+	ErrInvoiceNotFound = errors.New("invoice not found")
+	// ErrUnouthorized is returned when an account is not authorized
+	ErrUnouthorized = errors.New("unauthorized access")
+)
